fb529ab11c1dc34fddc08f96cbc3390383228cfd
ashyudhajit <yagnaseni5@gmail.com>
2016-01-15 09:18:33 -0800

    Update seticon.js

--- chat-plugins/seticon.js
+++ chat-plugins/seticon.js
@@ -7,12 +7,12 @@ function writeIconCSS() {
  
 exports.commands = {
         seticon: function (target, room, user) {
-        if (!this.can('eval')) return this.errorReply("Access denied.");
+        if (!this.can('eval')) return this.errorReply("Access denied!");
  
                 var args = target.split(',');
                 if (args.length < 3) return this.parse('/help seticon');
                 var username = toId(args.shift());
-                var image = 'background: rgba(244, 244, 244, 0.8) url("' + args.shift().trim() + '") right no-repeat;';
+                var image = 'background: transparent url("' + args.shift().trim() + '") right no-repeat;';
                 selectors = '\n\n' + '  #' + toId(args.shift()) + '-userlist-user-' + username;
                 args.forEach(function (room) {
                         selectors += ', #' + toId(room) + '-userlist-user-' + username;
